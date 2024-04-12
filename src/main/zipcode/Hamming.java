87915523c6ba76881430a1bbd61d643c63eb9390
Angelinamathew <angelinamathew707@gmail.com>
2024-04-11 23:38:04 -0400

    Updated

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,29 @@
 package zipcode;
 
+import java.util.Objects;
+
 public class Hamming {
+    String s;
+    String s1;
     public Hamming(String s, String s1) {
+        if (s.length() != s1.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length");
+        }
+        this.s = s;
+        this.s1 = s1;
     }
 
     public int getHammingDistance() {
-        return -1;
+        String[] sArray = s.split("");
+        String[] s1Array = s1.split("");
+
+        int hammingCounter = 0;
+
+        for (int i=0; i<s1.length(); i++){
+            if (!Objects.equals(sArray[i], s1Array[i])){
+                hammingCounter++;
+            }
+        }
+        return hammingCounter;
     }
 }
